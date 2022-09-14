564924b2d40cffd25422dafb161d356752a4c91f
Pawel W <elendil@konto.pl>
2022-09-14 22:31:45 +0200

    Refactor the unit tests of the CSignal

--- test/CSignalTest.cpp
+++ test/CSignalTest.cpp
@@ -1,84 +1,43 @@
 #include <gtest/gtest.h>
+#include <tuple>
 #include <CSignal.h>
 
-TEST( CSignal , BasicDeserialize_BigEndian )
+using tTestParameters = std::tuple<int, int, CSignal::eEndiannes, int>;
+
+class SignalTestsFixture : public ::testing::TestWithParam<tTestParameters>
 {
+
+};
+
+TEST_P( SignalTestsFixture, SignalValueExtraction)
+{
+  auto testParametersTuple = GetParam();
+
+  int start(std::get<0>(testParametersTuple));
+  int length(std::get<1>(testParametersTuple));
+  CSignal::eEndiannes endian(std::get<2>(testParametersTuple));
+  int expectedValue(std::get<3>(testParametersTuple));
+
+  CSignal signalProcessor(start,length,endian);
   uint64_t candata = 0x1032547698BADCFE;
   uint64_t reversedCanData = 0xfedcba9876543210;
-   CSignal signalProcessor(16,16, CSignal::bigEndian );
 
-  EXPECT_EQ( static_cast<unsigned int> (signalProcessor.ExtractValue(candata, reversedCanData,8)) , 0xDCBAUL) ;
+  EXPECT_EQ(static_cast<unsigned int>( signalProcessor.ExtractValue(candata, reversedCanData,8)), expectedValue);
+
 }
 
-TEST( CSignal , BasicDeserialize_LittleEndian )
-{
-  uint64_t candata = 0x1032547698BADCFE;
-  uint64_t reversedCanData = 0xfedcba9876543210;
-  CSignal signalProcessor(16,16, CSignal::littleEndian );
-  EXPECT_EQ( static_cast<unsigned int>(signalProcessor.ExtractValue(candata, reversedCanData, 8)) , 0x98BAUL) ;
-}
-
-TEST( CSignal , ShortDeserialize_BigEndian )
-{
-  uint64_t candata = 0x1032547698BADCFE;
-  uint64_t reversedCanData = 0xfedcba9876543210;
-  CSignal signalProcessor(18,4, CSignal::bigEndian );
-  EXPECT_EQ( static_cast<unsigned int> (signalProcessor.ExtractValue(candata, reversedCanData, 8) ) , 0x0E) ;
-}
-
-TEST( CSignal , ShortDeserialize_LittleEndian )
-{
-  uint64_t candata = 0x1032547698BADCFE;
-  uint64_t reversedCanData = 0xfedcba9876543210;
-  CSignal signalProcessor(18,4, CSignal::littleEndian );    
-  EXPECT_EQ( static_cast<unsigned int>(signalProcessor.ExtractValue(candata, reversedCanData, 8)) , 0x0E) ;
-}
-
-TEST( CSignal , ShortDeserialize_BigEndian_Boundary )
-{
-  uint64_t candata = 0x1032547698BADCFE;
-  uint64_t reversedCanData = 0xfedcba9876543210;
-  CSignal signalProcessor(8,16, CSignal::bigEndian );
-  EXPECT_EQ( static_cast<unsigned int> (signalProcessor.ExtractValue(candata, reversedCanData, 8) ) , 0xFEDC) ;
-}
-
-TEST( CSignal , ShortDeserialize_LittleEndian_Boundary )
-{
-  uint64_t candata = 0x1032547698BADCFE;
-  uint64_t reversedCanData = 0xfedcba9876543210;
-  CSignal signalProcessor(8,16, CSignal::littleEndian );    
-  EXPECT_EQ( static_cast<unsigned int>(signalProcessor.ExtractValue(candata, reversedCanData, 8)) , 0xBADC) ;
-}
-
-
-TEST( CSignal , ShortDeserializeBetweenBytes_BigEndian )
-{
-  uint64_t candata = 0x1032547698BADCFE;
-  uint64_t reversedCanData = 0xfedcba9876543210;
-  CSignal signalProcessor(30,6, CSignal::bigEndian );
-  EXPECT_EQ( static_cast<unsigned int> (signalProcessor.ExtractValue(candata, reversedCanData, 8) ) , 0x2A) ;
-}
-
-TEST( CSignal , ShortDeserializeBetweenBytes_LittleEndian )
-{
-  uint64_t candata = 0x1032547698BADCFE;
-  uint64_t reversedCanData = 0xfedcba9876543210;
-  CSignal signalProcessor(30,6, CSignal::littleEndian );
-  EXPECT_EQ( static_cast<unsigned int>(signalProcessor.ExtractValue(candata, reversedCanData, 8)) , 0x1A) ;
-}
-
-TEST( CSignal , LongDeserializeBetweenBytes_BigEndian )
-{
-  uint64_t candata = 0x1032547698BADCFE;
-  uint64_t reversedCanData = 0xfedcba9876543210;
-  CSignal signalProcessor(30,20, CSignal::bigEndian );
-  EXPECT_EQ( static_cast<unsigned int> (signalProcessor.ExtractValue(candata, reversedCanData, 8) ) , 0xB72EA) ;
-}
-
-TEST( CSignal , LongDeserializeBetweenBytes_LittleEndian )
-{
-  uint64_t candata = 0x1032547698BADCFE;
-  uint64_t reversedCanData = 0xfedcba9876543210;
-  CSignal signalProcessor(30,20, CSignal::littleEndian );
-  EXPECT_EQ( static_cast<unsigned int>(signalProcessor.ExtractValue(candata, reversedCanData, 8)) , 0x951DA) ;
-}
+INSTANTIATE_TEST_CASE_P(
+        CSignalTests,
+        SignalTestsFixture,
+        ::testing::Values(
+                tTestParameters({16,16, CSignal::bigEndian,0xDCBAUL} ),
+                tTestParameters({16,16, CSignal::littleEndian,0x98BAUL} ),
+                tTestParameters({18,4, CSignal::bigEndian,0x0E} ),
+                tTestParameters({18,4, CSignal::littleEndian,0x0E} ),
+                tTestParameters({8,16, CSignal::bigEndian,0xFEDC} ),
+                tTestParameters({8,16, CSignal::littleEndian,0xBADC} ),
+                tTestParameters({30,6, CSignal::bigEndian,0x2A} ),
+                tTestParameters({30,6, CSignal::littleEndian,0x1A} ),
+                tTestParameters({30,20, CSignal::bigEndian,0xB72EA} ),
+                tTestParameters({30,20, CSignal::littleEndian,0x951DA} )
+        ));
